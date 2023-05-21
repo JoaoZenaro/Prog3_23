9bcede16abb22349b212f6d52311d22166b60f16
JoaoZenaro <47560029+JoaoZenaro@users.noreply.github.com>
2023-05-21 22:30:07 +0000

    activ refactor

--- ativ02/Program.cs
+++ ativ02/Program.cs
@@ -6,110 +6,95 @@ string? input, input2;
 Console.WriteLine("Bem-vindo, Usuario!");
 Thread.Sleep(1000);
 
-do {
+do
+{
     menu(new string[] { "1. Cadastros", "2. Relatórios", "0. Sair" });
     input = Console.ReadLine() ?? "";
 
-    var actions = new Dictionary<string, Action> {
-        {
-            "1", () => {
-                path.Add("Cadastros");
-                do {
-                    menu(new string[] { "1. Médicos", "2. Animais", "3. Atendimentos", "4. Sair" });
-                    input2 = Console.ReadLine() ?? "";
+    switch (input)
+    {
+        case "1":
+            path.Add("Cadastros");
+            do
+            {
+                menu(new string[] { "1. Médicos", "2. Animais", "3. Atendimentos", "4. Sair" });
+                input2 = Console.ReadLine() ?? "";
 
-                    var subActions = new Dictionary<string, Action> {
-                        {
-                            "1", () => {
-                                Console.WriteLine("Médicos");
-                                pause();
-                            }
-                        },
-                        {
-                            "2", () => {
-                                Console.WriteLine("Animais");
-                                pause();
-                            }
-                        },
-                        {
-                            "3", () => {
-                                Console.WriteLine("Atendimentos");
-                                pause();
-                            }
-                        }
-                    };
+                switch (input2)
+                {
+                    case "1":
+                        Console.WriteLine("Médicos");
+                        pause();
+                        break;
+                    case "2":
+                        Console.WriteLine("Animais");
+                        pause();
+                        break;
+                    case "3":
+                        Console.WriteLine("Atendimentos");
+                        pause();
+                        break;
+                }
 
-                    if (subActions.TryGetValue(input2, out var subAction)) {
-                        subAction();
-                    }
+            } while (input2 != "4" && !string.IsNullOrEmpty(input2));
 
-                } while (!input2.Equals("4") && !string.IsNullOrEmpty(input2));
+            path.RemoveAt(path.Count - 1);
+            break;
 
-                path.RemoveAt(path.Count - 1);
-            }
-        },
-        {
-            "2", () => {
-                path.Add("Relatórios");
-                do {
-                    menu(new string[] { "1. Animais", "2. Atendimentos", "3. Sair" });
-                    input2 = Console.ReadLine() ?? "";
+        case "2":
+            path.Add("Relatórios");
+            do
+            {
+                menu(new string[] { "1. Animais", "2. Atendimentos", "3. Sair" });
+                input2 = Console.ReadLine() ?? "";
 
-                    var subActions = new Dictionary<string, Action> {
-                        {
-                            "1", () => {
-                                Console.WriteLine("Animais");
-                                pause();
-                            }
-                        },
-                        {
-                            "2", () => {
-                                Console.WriteLine("Atendimentos");
-                                pause();
-                            }
-                        }
-                    };
+                switch (input2)
+                {
+                    case "1":
+                        Console.WriteLine("Animais");
+                        pause();
+                        break;
+                    case "2":
+                        Console.WriteLine("Atendimentos");
+                        pause();
+                        break;
+                }
 
-                    if (subActions.TryGetValue(input2, out var subAction)) {
-                        subAction();
-                    }
+            } while (input2 != "3" && !string.IsNullOrEmpty(input2));
 
-                } while (!input2.Equals("3") && !string.IsNullOrEmpty(input2));
+            path.RemoveAt(path.Count - 1);
+            break;
 
-                path.RemoveAt(path.Count - 1);
-            }
-        },
-        {
-            "0", () => {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Saindo..");
-                Console.ForegroundColor = ConsoleColor.White;
-                Thread.Sleep(500);
-            }
-        }
-    };
+        case "0":
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Saindo..");
+            Console.ForegroundColor = ConsoleColor.White;
+            Thread.Sleep(500);
+            break;
 
-    if (actions.TryGetValue(input, out var action)) {
-        action();
-    } else {
-        Console.ForegroundColor = ConsoleColor.DarkRed;
-        Console.WriteLine("Opção Inválida!");
-        Console.ForegroundColor = ConsoleColor.White;
-        Thread.Sleep(750);
+        default:
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Opção Inválida!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Thread.Sleep(750);
+            break;
     }
 
-} while (!input.Equals("0") && !string.IsNullOrEmpty(input));
+} while (input != "0" && !string.IsNullOrEmpty(input));
 
-void menu(string[] arr) {
+void menu(string[] arr)
+{
     Console.Clear();
     Console.WriteLine($"Caminho atual: {String.Join(" > ", path.ToArray())} \n");
-    foreach (string i in arr) {
+    foreach (string i in arr)
+    {
         Console.WriteLine(i);
     }
     Console.Write("\nSelecione uma opção: ");
 }
 
-void pause() {
+void pause()
+{
     Console.WriteLine("\nPressione qualquer tecla para continuar...");
     Console.ReadKey();
 }
