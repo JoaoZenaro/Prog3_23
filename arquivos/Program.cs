f7bae563a6d10dcf55bd09426b20a49e4cb465b9
Joao Zenaro <joaomarcelo661@gmail.com>
2023-06-05 19:55:47 -0300

    aula 05_06 partial

---
+++ arquivos/Program.cs
@@ -0,0 +1,19 @@
+﻿int option;
+
+do
+{
+    Console.WriteLine(new String('*', 40));
+    Console.WriteLine("Programa para leitura e exportação de dados");
+    Console.WriteLine(new String('*', 40));
+    Console.WriteLine("1 - Clientes");
+
+    option = Convert.ToInt32(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1:
+            Console.WriteLine("Opção clientes");
+            break;
+    }
+
+} while (option != 0);
